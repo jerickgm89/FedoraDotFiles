8bfc037a6f0e7f8ebec19558e8e19d8b5c23068f
JErickGM <jerickgm89@gmail.com>
2021-01-24 14:27:54 -0500

    Script pad numerico

---
+++ PadNumerosKey.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# -*- ENCODING: UTF-8 -*-
+xmodmap -e "keycode  25 = w W w W 7" &
+xmodmap -e "keycode  26 = e E e E 8" &
+xmodmap -e "keycode  27 = r R r R 9" &
+xmodmap -e "keycode  39 = s S s S 4" &
+xmodmap -e "keycode  40 = d D d D 5" &
+xmodmap -e "keycode  41 = f F f F 6" &
+xmodmap -e "keycode  53 = x X x X 1" &
+xmodmap -e "keycode  54 = c C c C 2" &
+xmodmap -e "keycode  55 = v V v V 3" &
+xmodmap -e "keycode  52 = z Z z Z 0" &
+
+exit
--- script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# -*- ENCODING: UTF-8 -*-
-echo HolaMundo
